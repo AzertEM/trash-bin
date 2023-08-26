fcd22ea36d473d488fd4d75fe47e79b2e281ea51
jlee7196 <jlee7196@uni.sydney.edu.au>
2023-08-26 20:57:14 +1000

    plz debug

--- lib/photo.dart
+++ lib/photo.dart
@@ -1,18 +1,18 @@
-class photo {
-    late int? id = 0;
-    late String? photoName = "";
-    photo(int i, String imgString){
-        this.photoName = imgString;
-    }
-    Map <String, dynamic> toMap(){
-        var map = <String, dynamic>{
-                'id': id,
-                'photoName': photoName,
-};
-        return map;
-    }
-photo.fromMap(Map<String, dynamic>map){
-        id = map['id'];
-        photoName = map ['photoName'];
-    }
+class Photo {
+  late int? id = 0;
+  late String? photoName = "";
+  Photo(int i, String imgString){
+      this.photoName = imgString;
+  }
+  Map <String, dynamic> toMap(){
+    var map = <String, dynamic>{
+            'id': id,
+            'photoName': photoName,
+    };
+    return map;
+  }
+  Photo.fromMap(Map<String, dynamic>map){
+    id = map['id'];
+    photoName = map ['photoName'];
+  }
 }
--- lib/upload.dart
+++ lib/upload.dart
@@ -1,69 +1,8 @@
+import 'dart:io';
+ 
 import 'package:flutter/material.dart';
-import 'package:trash_bin/upload.dart';
-import 'database.dart';
 import 'package:image_picker/image_picker.dart';
-import 'photo.dart';
-import 'utility.dart';
-import 'map.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: HomePage(),
-        ),
-      ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePage();
-}
-
-
-class MyAppState extends ChangeNotifier {
-  var images = <Photo>[];
-
-  pickImageFromGallery() {
-    ImagePicker().pickImage(source: ImageSource.gallery).then((imgFile) async {
-        String imgString = Utility.base64String(await imgFile!.readAsBytes());
-        print(imgString);
-        Photo photo1 = Photo(0, imgString);
-        var dbHelper = DBHelper();
-        dbHelper.save(photo1);
-        // refreshImages();
-    });
-  }
-
-  gridView(){
-    return Padding(
-      padding: EdgeInsets.all(5.0),
-      child: GridView.count(
-        crossAxisCount: 2,
-        childAspectRatio: 1.0,
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
-        children: images.map((photo) {
-          print("photo name");
-          print(photo.photoName);
-          return Utility.imageFromBase64String(photo.photoName ?? "");
-        }).toList(),
-      ),
-    );
-  }
-}
 
  
 class GalleryAccess extends StatefulWidget {
@@ -175,46 +114,3 @@ class _GalleryAccessState extends State<GalleryAccess> {
     );
   }
 }
-class _HomePage extends State<HomePage> {
-  int currentPageIndex = 0;
-
-
-  @override
-  Widget build(BuildContext context) {
-    // ignore: unused_local_variable
-    Widget page;
-    switch (currentPageIndex) {
-      case 0:
-        page = const MapGenerator();
-        break;
-      case 1:
-        page = const GalleryAccess();
-      default:
-        throw UnimplementedError('Not implemented :<');
-    }
-    return Scaffold(
-      bottomNavigationBar: NavigationBar (
-          onDestinationSelected: (int index) {
-            setState(() {
-              currentPageIndex = index;
-            });
-          },
-          selectedIndex: currentPageIndex,
-          destinations: const [
-            NavigationDestination(
-              selectedIcon: Icon(Icons.map),
-              icon: Icon(Icons.map_outlined), 
-              label: 'Map',
-            ),
-            NavigationDestination(
-              selectedIcon: Icon(Icons.find_in_page),
-              icon: Icon(Icons.find_in_page_outlined), 
-              label: 'Find',
-            ),
-          ],
-        ),
-  
-      body: page,
-    );
-    }
-  }
