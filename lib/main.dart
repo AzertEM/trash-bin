c30ab973dc44514ece5818047a53cc364206c3c8
jlee7196 <jlee7196@uni.sydney.edu.au>
2023-08-26 20:27:32 +1000

    Added the database files--haven't tested it yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'database.dart';
+import 'package:image_picker/image_picker.dart';
+import 'photo.dart';
+import 'utility.dart';
 
 void main() {
   runApp(const MainApp());
@@ -26,6 +30,39 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePage();
 }
 
+
+class MyAppState extends ChangeNotifier {
+  var images = <Photo>[];
+
+  pickImageFromGallery() {
+    ImagePicker().pickImage(source: ImageSource.gallery).then((imgFile) async {
+        String imgString = Utility.base64String(await imgFile!.readAsBytes());
+        print(imgString);
+        Photo photo1 = Photo(0, imgString);
+        var dbHelper = DBHelper();
+        dbHelper.save(photo1);
+        // refreshImages();
+    });
+  }
+
+  gridView(){
+    return Padding(
+      padding: EdgeInsets.all(5.0),
+      child: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+        children: images.map((photo) {
+          print("photo name");
+          print(photo.photoName);
+          return Utility.imageFromBase64String(photo.photoName ?? "");
+        }).toList(),
+      ),
+    );
+  }
+}
+
 class _HomePage extends State<HomePage> {
   int currentPageIndex = 0;
 
@@ -33,25 +70,25 @@ class _HomePage extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: NavigationBar (
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        selectedIndex: currentPageIndex,
-        destinations: const [
-          NavigationDestination(
-            selectedIcon: Icon(Icons.map),
-            icon: Icon(Icons.map_outlined), 
-            label: 'Map',
-          ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.find_in_page),
-            icon: Icon(Icons.find_in_page_outlined), 
-            label: 'Find',
-          ),
-        ],
-      ),
-    );
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          selectedIndex: currentPageIndex,
+          destinations: const [
+            NavigationDestination(
+              selectedIcon: Icon(Icons.map),
+              icon: Icon(Icons.map_outlined), 
+              label: 'Map',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.find_in_page),
+              icon: Icon(Icons.find_in_page_outlined), 
+              label: 'Find',
+            ),
+          ],
+        ),
+      );
+    }
   }
-}
