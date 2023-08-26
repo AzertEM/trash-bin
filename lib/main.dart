9ef45e64ad9186e69de64b2a2e810abf1439748e
Huan Vo <hovo3634@uni.sydney.au.edu>
2023-08-26 14:47:50 +1000

    Added Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,45 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: HomePage(),
         ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePage();
+}
+
+class _HomePage extends State<HomePage> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar (
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: const [
+          NavigationDestination(
+            selectedIcon: Icon(Icons.map),
+            icon: Icon(Icons.map_outlined), 
+            label: 'Map',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.find_in_page),
+            icon: Icon(Icons.find_in_page_outlined), 
+            label: 'Find',
+          ),
+        ],
       ),
     );
   }
