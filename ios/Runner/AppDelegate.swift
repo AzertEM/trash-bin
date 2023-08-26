169191953e09ad823248434d1fb381f04ae86087
Huan Vo <hovo3634@uni.sydney.au.edu>
2023-08-26 17:22:55 +1000

    Now with a map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    GMSServices.provideAPIKey("AIzaSyDycs7k2-3gt-l328GI94FsmIJMK2u6jaA")
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
